ac94a3efb29bc4a0625b5c834183dcc56a9ebbed
BrianCrosser <bacrosser@sbcglobal.net>
2015-09-09 13:23:34 -0700

    switch case updated

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main()
     vector<Elevator> elevatorList;
     Elevator E;
     while(runBool) {
-        cout << "Commands: (1) Add a level, (2) Add an elevator, (3) Print stats of elevator, (4) exit program" << endl;
+        cout << "Commands: (1) Add a level, (2) Add an elevator, (4) Print stats of elevator to screen, (5) exit program" << endl;
         cout << "Please give a command!: " << endl;
         cin >> userInput;
         switch (userInput) {
@@ -21,10 +21,10 @@ int main()
             case 2:
                 E.createElevator(elevatorList);
                 break;
-            case 3:
+            case 4:
                 E.printStats(elevatorList);
                 break;
-            case 4:
+            case 5:
                 runBool = false;
                 break;
         }
